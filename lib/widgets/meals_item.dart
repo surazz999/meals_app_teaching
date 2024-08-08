1a63e0e2851caad34fcd9a051b62b447b77725e0
surazz999 <surazsub999@gmail.com>
2024-08-08 10:35:29 +0545

    design

--- lib/widgets/meals_item.dart
+++ lib/widgets/meals_item.dart
@@ -8,13 +8,24 @@ class MealItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      clipBehavior: Clip.hardEdge,
+      elevation: 2,
       child: InkWell(
         onTap: () {},
         child: Stack(
           children: [
             FadeInImage(
               placeholder: MemoryImage(kTransparentImage),
-              image: NetworkImage(meal.imageUrl),
+              image: NetworkImage(
+                meal.imageUrl,
+              ),
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
             ),
             Positioned(
               bottom: 0,
